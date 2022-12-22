dc193a15b52dc22a008eb0ae174cf66bfc2778a4
gzlock <gzlock88@gmail.com>
2022-12-23 06:22:53 +0800

    v0.9.4

--- src/content/main.ts
+++ src/content/main.ts
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './components/app.vue'
+// 立即刷新页面菜单
+document.addEventListener('mousedown', (e) => {
+  if (e.button == 2) {
+    chrome.runtime.sendMessage(
+      'contextmenu',
+      (res) => {
+        console.log('simple-extension update menu', res)
+      },
+    )
+  }
+})
 
-createApp(App).mount('#app')
+// 更新数据
+document.addEventListener('load', () => {
+  chrome.runtime.sendMessage('update')
+})
